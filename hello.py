0284c37e1c41eaa1302b5d48a3d043851e0fe579
Michael <mf@mifi.tips>
2018-12-04 14:02:24 +0100

    changed tab stops with spaces

--- hello.py
+++ hello.py
@@ -31,30 +31,30 @@ class window (JPanel):
         self.frame.setLayout(None)
         self.pane = self.frame.getContentPane()
         self.pane.setBackground(Color(255,255,225))
-    
+
         self.btn = JButton("Add", actionPerformed = self.actionPerformed, bounds = (20,20,100,20))
         self.btn.setActionCommand("add")
         self.pane.add(self.btn)
-        
+
         self.btn2 = JButton("Delete", actionPerformed = self.actionPerformed)
         self.btn2.setActionCommand("delete")
         self.btn2.setBounds(20, 50,100,20)
         self.pane.add(self.btn2)
-        
+
         self.quitbtn = JButton("Quit", actionPerformed = self.actionPerformed)
         self.quitbtn.setActionCommand("quit")
         self.quitbtn.setBounds(20, 80, 100, 20)
         self.pane.add(self.quitbtn)
-        
-        
+
+
         self.b = BorderFactory.createLineBorder(Color(0,0,0), 1)
         self.text = JLabel("")
-	self.text.setBounds(20,110,200,20)
-	self.text.setBorder(self.b)
+        self.text.setBounds(20,110,200,20)
+        self.text.setBorder(self.b)
         self.pane.add(self.text)
-        
+
         self.frame.visible = True
-                
+
 
 print ("Test")
 w = window("Hello World")
